d11f2b98103afad9f226d7999e28bc656bc45765
Kuldeep12mohan <mohankuldeep1234@gmail.com>
2025-07-03 16:58:20 +0530

    port configuration

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 const mainRouter = require("./routes/index");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1",mainRouter);
-
-app.listen(3000,()=>
+const PORT = process.env.PORT
+app.listen(PORT,()=>
 {
   console.log("app is listening on port 3000");
 });
